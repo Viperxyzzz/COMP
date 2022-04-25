547164c15e54a8c03ebf762a3ffaa9f58d94917f
up201905407 <up201905407@fe.up.pt>
2022-04-25 21:58:10 +0100

    more on visitors

--- src/pt/up/fe/comp/AstVisitor.java
+++ src/pt/up/fe/comp/AstVisitor.java
@@ -11,6 +11,8 @@ public class AstVisitor extends PreorderJmmVisitor<List<String>,Boolean> {
         addVisit("ImportDeclaration", this::visitImport);
         addVisit("ClassDecl", this::visitClass);
         addVisit("ExtendsExp", this::visitExtends);
+        addVisit("MainMethod", this::visitMain);
+        addVisit("MethodDecl", this::visitMethod);
     }
 
     private Boolean visitImport(JmmNode importDecl, List<String> imports){
@@ -32,6 +34,32 @@ public class AstVisitor extends PreorderJmmVisitor<List<String>,Boolean> {
 
     private Boolean visitExtends(JmmNode extendsExp, List<String> extendsValue){
         extendsValue.add(extendsExp.get("value"));
+
+        return true;
+    }
+
+    private Boolean visitMain(JmmNode mainMethod, List<String> args){
+        var argString = mainMethod.getChildren().stream()
+                .map(Param -> Param.get("value"))
+                .collect(Collectors.joining("."));
+
+        args.add(argString);
+
+        return true;
+    }
+
+    private Boolean visitMethod(JmmNode methodDecl, List<String> args){
+        //var methodType = ;
+
+        //var methodName = ;
+
+        var argString = methodDecl.getChildren().stream()
+                .map(Param -> Param.get("value"))
+                .collect(Collectors.joining("."));
+
+
+
+        args.add(argString);
 
         return true;
     }
